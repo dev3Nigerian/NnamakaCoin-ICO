80274e1e451ae411b2a6528a719afca0738f92bb
dev3Nigerian <faithfulodigbo@gmail.com>
2022-05-12 11:07:12 +0100

    added js file for deploy

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// address of NFT contract deploy in prev. repo
+const ODIGBO_NFT_CONTRACT_ADDRESS = "0x92dc9443CEEc89Ed75c863e2c15798EAA8aB9c83";
+
+module.exports = { ODIGBO_NFT_CONTRACT_ADDRESS };
